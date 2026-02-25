479b8b1ce7fb6958d3de3cc8bc245f19c21af906
chongtcums <131859140+chongtcums@users.noreply.github.com>
2026-02-25 09:53:47 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,27 @@
 using System;
-using Newtonsoft.Json;
+using System.Data.SqlClient;
 
 class Program
 {
     static void Main()
     {
-        var obj = new { Name = "SecurityTest" };
-        string json = JsonConvert.SerializeObject(obj);
-        Console.WriteLine(json);
+        Console.Write("Enter username: ");
+        string username = Console.ReadLine();
+
+        string connectionString = "Server=myServer;Database=myDB;User Id=sa;Password=password;";
+        using (SqlConnection conn = new SqlConnection(connectionString))
+        {
+            conn.Open();
+
+            // 🚨 SQL Injection Vulnerability
+            string query = "SELECT * FROM Users WHERE Username = '" + username + "'";
+            SqlCommand cmd = new SqlCommand(query, conn);
+
+            var reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine("User found");
+            }
+        }
     }
 }
